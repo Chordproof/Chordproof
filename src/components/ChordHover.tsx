28da4a473b617825c13e9d8f1b422861fad1eeb4
Chordproof <direitohenrique@hotmail.com>
2026-07-30 11:40:31 -0300

    Add ChordHover component for displaying chord diagrams

---
+++ src/components/ChordHover.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState } from "react";
+import ChordDiagram from "./ChordDiagram";
+
+interface ChordHoverProps {
+  chord: string;
+  children: React.ReactNode;
+}
+
+export default function ChordHover({ chord, children }: ChordHoverProps) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <span
+      className="relative inline-block cursor-pointer"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      <span className="chord">{children}</span>
+      {show && (
+        <div className="absolute z-50 bottom-full left-1/2 -translate-x-1/2 mb-2">
+          <ChordDiagram chord={chord} frets={[]} fingers={[]} />
+        </div>
+      )}
+    </span>
+  );
+}
