71803cba2be86186efdf3b84bf2acc55bf9ac22b
Chordproof <direitohenrique@hotmail.com>
2026-07-31 11:04:53 -0300

    Replace createBrowserClient with createPagesBrowserClient

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,9 +1,6 @@
 "use client";
-import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 
 export function createClientSupabaseClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  return createPagesBrowserClient();
 }
