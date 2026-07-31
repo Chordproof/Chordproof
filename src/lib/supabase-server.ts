64d3602987dd3d9e10ea5e6eede46a551543f72a
Chordproof <direitohenrique@hotmail.com>
2026-07-31 11:13:52 -0300

    Refactor to use createPagesServerClient

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -1,16 +1,6 @@
-import { createServerClient } from "@supabase/auth-helpers-nextjs";
+import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 export function createServerSupabaseClient() {
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookies().get(name)?.value;
-        },
-      },
-    }
-  );
+  return createPagesServerClient({ cookies });
 }
