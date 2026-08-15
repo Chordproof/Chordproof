26f42d0e1d67a173800cbd67ad6905a6f30b7f2c
Chordproof <direitohenrique@hotmail.com>
2026-08-15 18:20:05 -0300

    Add JsonLd component for structured data

---
+++ src/components/JsonLd.tsx
@@ -0,0 +1,9 @@
+// src/components/JsonLd.tsx
+export default function JsonLd({ data }: { data: object }) {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}
