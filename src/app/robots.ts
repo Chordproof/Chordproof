aa88a15b862245f8f5745142c5050f04b3403378
Chordproof <direitohenrique@hotmail.com>
2026-08-15 18:27:35 -0300

    Add robots.txt generation function

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+// src/app/robots.ts
+import { SITE_URL } from "@/lib/seo";
+
+export default function robots() {
+  return {
+    rules: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "*", disallow: ["/signin", "/account"] },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
+  };
+}
